6b2ddd05a314d2fa4fd47cd2f11563ecd1bd74a5
Investgrape <alex@investgrape.com>
2024-12-26 08:56:39 -0800

    Update project configuration

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ria-back-office/'
+  base: '/ria-back-office/',
+  build: {
+    outDir: 'dist',
+    sourcemap: true
+  }
 })
