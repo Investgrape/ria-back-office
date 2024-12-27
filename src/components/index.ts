e36aa47680c6d83c5438b14ba22543ed819ab7bb
Investgrape <alex@investgrape.com>
2024-12-26 23:40:02 -0800

    fix: add component index exports and clean up structure

---
+++ src/components/index.ts
@@ -0,0 +1,11 @@
+export * from './ui/card';
+export * from './ui/sidebar';
+export * from './ui/sidebar-nav';
+
+export { default as DashboardContent } from './DashboardContent';
+export { default as DocumentsSection } from './DocumentsSection';
+export { default as EmailSection } from './EmailSection';
+export { default as EmployeeSection } from './EmployeeSection';
+export { default as MarketingSection } from './MarketingSection';
+export { default as AuditSection } from './AuditSection';
+export { default as ClientAgreements } from './ClientAgreements';
