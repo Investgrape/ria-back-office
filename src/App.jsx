f5c2ecf9463e64ff8a3fe9858438eb095ad3b457
Investgrape <alex@investgrape.com>
2024-12-26 19:48:03 -0800

    Update ClientAgreementsPage and routing with new agreement functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
-import Dashboard from './components/Dashboard';
-import ClientAgreements from './components/ClientAgreements/ClientAgreements';
+import DocumentsSection from './components/DocumentsSection';
+import EmailSection from './components/EmailSection';
+import EmployeeSection from './components/EmployeeSection';
+import MarketingSection from './components/MarketingSection';
+import AuditSection from './components/AuditSection';
+import ClientAgreementsPage from './components/ClientAgreementsPage';
 
-function App() {
-  return (
-    <Router basename="/ria-back-office">
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/agreements" element={<ClientAgreements />} />
-          <Route path="/employees" element={<div>Employees</div>} />
-          <Route path="/audit" element={<div>Audit</div>} />
-          <Route path="/documents" element={<div>Documents</div>} />
-          <Route path="/marketing" element={<div>Marketing</div>} />
-          <Route path="/email" element={<div>Email</div>} />
-        </Routes>
-      </Layout>
-    </Router>
-  );
-}
-
-export default App;
+// App implementation...
