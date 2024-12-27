ee5e139d9ff556a21a23016a6871c8772bc7bd68
Investgrape <alex@investgrape.com>
2024-12-26 19:52:45 -0800

    Update App.jsx to include agreements route while preserving existing structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
+import DashboardContent from './components/DashboardContent';
 import DocumentsSection from './components/DocumentsSection';
 import EmailSection from './components/EmailSection';
 import EmployeeSection from './components/EmployeeSection';
@@ -13,13 +14,13 @@ const App = () => {
     <Router>
       <Layout>
         <Routes>
+          <Route path="/" element={<DashboardContent />} />
           <Route path="/documents" element={<DocumentsSection />} />
           <Route path="/email" element={<EmailSection />} />
           <Route path="/employees" element={<EmployeeSection />} />
           <Route path="/marketing" element={<MarketingSection />} />
           <Route path="/audit" element={<AuditSection />} />
           <Route path="/agreements" element={<ClientAgreements />} />
-          <Route path="/" element={<DocumentsSection />} />
         </Routes>
       </Layout>
     </Router>
