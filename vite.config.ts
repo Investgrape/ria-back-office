a1b871e0e7c4e29a1208b8718ffe055a9b21c776
Investgrape <alex@investgrape.com>
2024-12-28 09:38:45 -0800

    fix: update vite config for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/ria-back-office/',
+  build: {
+    outDir: 'docs',
+    emptyOutDir: true
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
