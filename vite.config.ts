e95589037355fedcbf6bcfbe11ff47eeac263d52
Investgrape <alex@investgrape.com>
2024-12-27 21:58:19 -0800

    Update core configuration files

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/ria-back-office/',
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          'react-vendor': ['react', 'react-dom', 'react-router-dom'],
-          'ui-vendor': ['@radix-ui/react-dialog', '@radix-ui/react-label', 'lucide-react']
-        }
-      }
-    }
-  }
-})
+});
