28736ea50797da34a627fc714d18530e2ec899f4
Investgrape <alex@investgrape.com>
2024-12-28 09:47:18 -0800

    fix: update App.tsx with correct router setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,12 @@
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout/Layout';
-import Dashboard from './components/Dashboard/Dashboard';
-import AgreementManagement from './components/Agreements/AgreementManagement';
+import { BrowserRouter } from 'react-router-dom';
 
-const App = () => {
+function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Dashboard />} />
-          <Route path="dashboard" element={<Dashboard />} />
-          <Route path="agreements" element={<AgreementManagement />} />
-        </Route>
-      </Routes>
-    </Router>
+    <BrowserRouter basename="/ria-back-office">
+      <Layout />
+    </BrowserRouter>
   );
-};
+}
 
 export default App;
